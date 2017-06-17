85c09798428a4cfe6db016663fa9e0eaabcff75d
sarthak-97 <sarthakgupta115@gmail.com>
2017-06-17 22:28:26 +0530

    GET and POST api added

--- src/main/java/org/javabrains/msngr/resources/MessageResource.java
+++ src/main/java/org/javabrains/msngr/resources/MessageResource.java
@@ -2,8 +2,11 @@ package org.javabrains.msngr.resources;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -17,9 +20,32 @@ public class MessageResource {
 	
 	
 	@GET
-	@Produces(MediaType.APPLICATION_XML)
+	@Produces(MediaType.APPLICATION_JSON)
 	public List<Message> getMessage(){
 		
 		return msgservice.getallMessages();
 	}
+	
+	/**
+	 * 
+	 * @param id
+	 * @return
+	 */
+	@GET
+	@Path("/{messageId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message getMessage(@PathParam("messageId") long id){
+		
+		return msgservice.getMessage(id);
+		
+	}
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message addMessage(Message message){
+		
+		return msgservice.addMessage(message);
+	}
+	
 }
